61356cb3871f327e74826d651e93be3fab60e98a
han <igoman2@ajou.ac.kr>
2021-03-14 23:46:16 +0900

    insert image

--- Ch1/btn_tutorial/btn_tutorial/ViewController.swift
+++ Ch1/btn_tutorial/btn_tutorial/ViewController.swift
@@ -8,25 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue = 0 ;
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        refresh()
         // Do any additional setup after loading the view.
     }
     //button Action
     @IBAction func actionBtn(_ sender: Any) {
-        let alert = UIAlertController(title:"Hello", message: "My First App", preferredStyle: .alert)
-        let action = UIAlertAction(title:"OK", style: .default, handler: nil)
+        
+
+//      == let message: String
+        let message = "가격은 W\(currentValue) 입니다"
+        let alert = UIAlertController(title:"Hello", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title:"OK", style: .default, handler: { action in
+        self.refresh()
+        })
         alert.addAction(action)
         present(alert ,animated: true, completion: nil)
     }
     
-    @IBAction func actionBtn2(_ sender: Any) {
-        let alert = UIAlertController(title:"도오오오전", message: "아아아아", preferredStyle: .alert)
-        let action = UIAlertAction(title:"OK", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert ,animated: true, completion: nil)
+    func refresh() {
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        priceLabel.text = "W\(currentValue)"
+    
     }
-
 }
 
